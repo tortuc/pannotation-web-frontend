05cdb109a1872e3f1af15c12c8dde987ed798409
TortuC <tortuc611@gmail.com>
2021-06-15 10:50:47 +0800

    git init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true
+};
+
+export const API_URL = 'https://api.pannotation.com/api/v1';
+export const FacebookSocialConfig = {
+    appId: 'XXX',
+    xfbml: true,
+    version: 'v3.2'
+};
+
+export const GOOGLE_CLIENT_ID = 'XXX';
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false
+};
+
+export const API_URL = 'http://192.168.110.58:8085/api/v1';
+export const FacebookSocialConfig = {
+    appId: 'XXX',
+    xfbml: true,
+    version: 'v3.2'
+};
+
+export const GOOGLE_CLIENT_ID = 'XXX';
